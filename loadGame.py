f7117cdd35a81540a7ad9fd80d93ea04a398dce3
kevin <kevin.mazure.km@gmail.com>
2021-07-18 16:53:34 +0200

    préparation loadGame

--- loadGame.py
+++ loadGame.py
@@ -1,3 +1,30 @@
 # va lire et récupérer les données d'une sauvegarde
-def loadGame():
-    print("Votre partie charge")
+import json
+
+from game import setValues
+
+
+def loadGame(id):
+
+    game = {}
+    file = open("Save.json")
+    data = json.load(file)
+    for txt in data['scenario']:
+        if txt['id'] == id:
+            game = {
+                "wood": txt['game']['wood'],
+                "iron": txt['game']['iron'],
+                "food": txt['game']['food'],
+                "people": txt['game']['people'],
+                "houses": txt['game']['houses'],
+                "dateTime": 0,
+                "satisfaction": txt['game']['satisfaction'],
+                "levels": {
+                    "wood": txt['game']['levels']['wood'],
+                    "iron": txt['game']['levels']['iron'],
+                    "food": txt['game']['levels']['food']
+                }
+            }
+    dif = 3
+    setValues(game, dif)
+    file.close()
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+
 from loadGame import loadGame
 from newGame import newGame
 
@@ -15,7 +17,19 @@ def start():
         if i == 1:
             newGame()
         elif i == 2:
-            loadGame()
+            #lui permettra de choisir la save qu'il veut
+            #j = 1;
+            #print("Choisissez un scénario: ")
+            #file = open("Scenario.json")
+            #data = json.load(file)
+            #for txt in data['scenario']:
+             #   print(j, txt['name'])
+              #  j += 1
+
+            #file.close()
+            #id = int(input())
+            #loadGame(id)
+            newGame()
         else:
             print("Mettez un nombre entre 1 et 2")
 if __name__ == '__main__':
